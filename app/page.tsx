2f7d937f1f3a26c54cced567af04b45809a8aaaa
Eragon67360 <thomas-moser@orange.fr>
2024-04-12 19:20:50 +0200

    Added inline style and replaced string with number

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ import Sakana from "@/components/Logos/Sakana";
 import smurfit from '@/public/images/smurfit.png';
 
 import Card from "@/components/Card";
-const contentAresDomus = ['Portfolio_cristina_page-0004.jpg','Portfolio_cristina_page-0005.jpg','Portfolio_cristina_page-0006.jpg']
-const contentMoca = ['Portfolio_cristina_page-0007.jpg','Portfolio_cristina_page-0008.jpg']
-const contentMontezuma = ['Portfolio_cristina_page-0009.jpg','Portfolio_cristina_page-0010.jpg','Portfolio_cristina_page-0011.jpg','Portfolio_cristina_page-0012.jpg','Portfolio_cristina_page-0013.jpg']
-const contentSmurfkit = ['Portfolio_cristina_page-0014.jpg','Portfolio_cristina_page-0015.jpg','Portfolio_cristina_page-0016.jpg','Portfolio_cristina_page-0017.jpg']
-const contentSakana = ['Portfolio_cristina_page-0018.jpg','Portfolio_cristina_page-0019.jpg','Portfolio_cristina_page-0020.jpg']
+const contentAresDomus = ['Portfolio_cristina_page-0004.jpg', 'Portfolio_cristina_page-0005.jpg', 'Portfolio_cristina_page-0006.jpg']
+const contentMoca = ['Portfolio_cristina_page-0007.jpg', 'Portfolio_cristina_page-0008.jpg']
+const contentMontezuma = ['Portfolio_cristina_page-0009.jpg', 'Portfolio_cristina_page-0010.jpg', 'Portfolio_cristina_page-0011.jpg', 'Portfolio_cristina_page-0012.jpg', 'Portfolio_cristina_page-0013.jpg']
+const contentSmurfkit = ['Portfolio_cristina_page-0014.jpg', 'Portfolio_cristina_page-0015.jpg', 'Portfolio_cristina_page-0016.jpg', 'Portfolio_cristina_page-0017.jpg']
+const contentSakana = ['Portfolio_cristina_page-0018.jpg', 'Portfolio_cristina_page-0019.jpg', 'Portfolio_cristina_page-0020.jpg']
 
 
 export default function Home() {
@@ -21,15 +21,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
 
       <div className="max-w-[1440px] w-[1440px] grid grid-cols-3">
-        <Card bg_color={"#E2E2DB"} logo={<MainLogo />} name={"Next Project"} name_color={"black"} padding_top={"132"} content={contentAresDomus} />
-        <Card bg_color={"#E2E2DB"} logo={<MainLogo />} name={"Next Project"} name_color={"black"} padding_top={"132"} content={contentAresDomus} />
-        <Card bg_color={"#E2E2DB"} logo={<MainLogo />} name={"Next Project"} name_color={"black"} padding_top={"132"} content={contentAresDomus} />
-        <Card bg_color={"#000"} logo={<Curefab />} name={"Website redesign"} name_color={"white"} padding_top={"192"} content={contentAresDomus} />
-        <Card bg_color={"#597177"} logo={<Sakana />} name={"Swiss watch design"} name_color={"white"} padding_top={"124"} content={contentSakana} />
-        <Card bg_color={"#FFF"} logo={<Image src={smurfit} alt="smurfit logo" width={279} height={79} />} name={"Packaging design"} name_color={"black"} padding_top={"196"} content={contentSmurfkit} />
-        <Card bg_color={"#91A399"} logo={<Montezuma />} name={"Marketing strategy + packaging design"} name_color={"black"} padding_top={"156"} content={contentMontezuma} />
-        <Card bg_color={"#718B75"} logo={<Moca />} name={"Brand design"} name_color={"white"} padding_top={"140"} content={contentMoca} />
-        <Card bg_color={"#1D252D"} logo={<AresDomus />} name={"Brand design"} name_color={"white"} padding_top={"132"} content={contentAresDomus} />      
+        <Card bg_color={"#E2E2DB"} logo={<MainLogo />} name={"Next Project"} name_color={"black"} padding_top={132} content={contentAresDomus} />
+        <Card bg_color={"#E2E2DB"} logo={<MainLogo />} name={"Next Project"} name_color={"black"} padding_top={132} content={contentAresDomus} />
+        <Card bg_color={"#E2E2DB"} logo={<MainLogo />} name={"Next Project"} name_color={"black"} padding_top={132} content={contentAresDomus} />
+
+        <Card bg_color={"#000"} logo={<Curefab />} name={"Website redesign"} name_color={"white"} padding_top={192} content={contentAresDomus} />
+        <Card bg_color={"#597177"} logo={<Sakana />} name={"Swiss watch design"} name_color={"white"} padding_top={124} content={contentSakana} />
+        <Card bg_color={"#FFF"} logo={<Image src={smurfit} alt="smurfit logo" width={279} height={79} />} name={"Packaging design"} name_color={"black"} padding_top={196} content={contentSmurfkit} />
+        <Card bg_color={"#91A399"} logo={<Montezuma />} name={"Marketing strategy + packaging design"} name_color={"black"} padding_top={156} content={contentMontezuma} />
+        <Card bg_color={"#718B75"} logo={<Moca />} name={"Brand design"} name_color={"white"} padding_top={140} content={contentMoca} />
+        <Card bg_color={"#1D252D"} logo={<AresDomus />} name={"Brand design"} name_color={"white"} padding_top={132} content={contentAresDomus} />
       </div>
     </main>
   );
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,25 +2,33 @@
 import React, { useState } from 'react'
 import Modal from "@/components/Modal";
 
-const Card = ({ bg_color, logo, name, name_color, padding_top, content }: { bg_color: string, logo: any, name: string, name_color: string, padding_top: string, content:string[] }) => {
+const Card = ({ bg_color, logo, name, name_color, padding_top, content }: { bg_color: string, logo: any, name: string, name_color: string, padding_top: number, content: string[] }) => {
     const [isModalOpen, setModalOpen] = useState(false);
 
     return (
         <>
-            <div className={`flex flex-col items-center justify-between pt-[${padding_top}px] pb-[92px] gap-8 h-[480px] bg-[${bg_color}] group cursor-pointer`} onClick={() => setModalOpen(true)}>
+            <div
+                style={{
+                    paddingTop: `${padding_top}px`,
+                    paddingBottom: '92px',
+                    backgroundColor: bg_color,
+                    height: '480px'
+                }}
+                className="flex flex-col justify-between items-center gap-8 group cursor-pointer"
+                onClick={() => setModalOpen(true)}>
                 <div className="flex items-center justify-center group-hover:scale-105 transition-all duration-200">{logo}</div>
                 <h2 className={`uppercase text-xs tracking-[6px] text-center text-${name_color}`}>{name}</h2>
             </div>
 
             <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
                 <div className='flex flex-col'>
-                {content.map((link, index)=>{
-                    return (
-                        <img src={`/images/projets/${link}`} alt={'Project content'} key={index} className="mb-4 max-w-full h-auto"/>
-                    )
-                })}
+                    {content.map((link, index) => {
+                        return (
+                            <img src={`/images/projets/${link}`} alt={'Project content'} key={index} className="mb-4 max-w-full h-auto" />
+                        )
+                    })}
                 </div>
-                
+
             </Modal>
         </>
 
